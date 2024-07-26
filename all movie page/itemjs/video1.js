e1d5edbde02fbef557ab2b7ebf4843ca34a060b4
RAM LAKHAN <ramlakhanmeena7798@gmail.com>
2024-07-26 14:38:46 +0530

    Update video1.js

--- all movie page/itemjs/video1.js
+++ all movie page/itemjs/video1.js
@@ -1,9 +1,9 @@
 const items = [
     {   
         id:'',
-        tittle: '',
-        smalltittle: '',
-        banner: '',
+        tittle: ' Maidaan (2024) WEB-DL [Hindi DD5.1] 4K 1080p 720p & 480p [x264/HEVC] | Full Movie',
+        smalltittle: 'Maidaan (2024) Hindi WEB-DL 4K 1080p 720p & 480p x264 DD5.1 | [Drama] Full Movie',
+        banner: './all movie page/VIDEO BANNER',
         pagename: './page.html',
         screeshort1: '',
         screeshort2: '',
