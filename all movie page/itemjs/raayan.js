b4e719c1421682a979d956c9ee79c23e49f41dd2
RAM LAKHAN <ramlakhanmeena7798@gmail.com>
2024-07-27 09:05:24 +0530

    Update raayan.js

--- all movie page/itemjs/raayan.js
+++ all movie page/itemjs/raayan.js
@@ -12,8 +12,8 @@ const items = [
         screeshort4: './all movie page/screenshot/raayan/126.jpg',
         screeshort5: './all movie page/screenshot/raayan/127.jpg',
         screeshort6: './all movie page/screenshot/raayan/128.jpg',
-        videodownloadlink: '',
-        watchonlinelink: '',
+        videodownloadlink: 'https://vidhidepre.com/file/5qryecgea4u5',
+        watchonlinelink: 'https://vidhidepre.com/file/5qryecgea4u5',
         watchtrailor: '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/qQJJWhh-XRo?si=HdMLrPEjcQ1rcy8x" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
         
 
