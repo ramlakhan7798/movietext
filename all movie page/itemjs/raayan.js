066a789e12aeadda1b0cec6c36631a5cf89ca99c
RAM LAKHAN <ramlakhanmeena7798@gmail.com>
2024-07-27 08:37:36 +0530

    Add files via upload

---
+++ all movie page/itemjs/raayan.js
@@ -0,0 +1,28 @@
+const items = [
+    {   
+        id:'01',
+        tittle: 'Raayan (2024) HDTS Hindi (LiNE) 1080p 720p & 480p [x264/HEVC] | Full Movie',
+        smalltittle: 'Raayan (2024) Hindi Dubbed HDTS 1080p 720p & 480p x264 | [Action] Full Movie.',
+        smalllow:'Raayan',
+        banner: './all movie page/VIDEO BANNER/raayan/123.jpg',
+        pagename: './raayan.html',
+        screeshort1: './all movie page/screenshot/raayan/123.jpg',
+        screeshort2: './all movie page/screenshot/raayan/124.jpg',
+        screeshort3: './all movie page/screenshot/raayan/125.jpg',
+        screeshort4: './all movie page/screenshot/raayan/126.jpg',
+        screeshort5: './all movie page/screenshot/raayan/127.jpg',
+        screeshort6: './all movie page/screenshot/raayan/128.jpg',
+        videodownloadlink: '',
+        watchonlinelink: '',
+        watchtrailor: '',
+        
+
+
+    }
+
+
+
+
+
+
+]
